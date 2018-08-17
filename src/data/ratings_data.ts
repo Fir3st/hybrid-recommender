74a1f83e0704d1fa33df2165a303eb8937858f40
Vladimír Fojtík <fojtik.v@gmail.com>
2018-08-17 10:30:01 +0200

    Divided ratings data to ratings by user and ratings by movie

--- src/data/ratings_data.ts
+++ src/data/ratings_data.ts
@@ -1,7 +1,10 @@
 import * as csv from 'csvtojson';
 import * as fs from 'fs';
 
-const ratings = {
+const ratingsByUser = {
+    data: {}
+};
+const ratingsByMovie = {
     data: {}
 };
 
@@ -9,21 +12,40 @@ csv()
     .fromFile('src/data/ratings.csv')
     .then((data: any) => {
         data.forEach((element) => {
-            if (element.userId in ratings.data) {
-                ratings.data[element.userId][element.movieId] = {
-                    rating: Number.parseFloat(element.rating)
+            if (element.userId in ratingsByUser.data) {
+                ratingsByUser.data[element.userId][element.movieId] = {
+                    rating: Number.parseFloat(element.rating),
+                    timestamp: element.timestamp
                 };
             } else {
-                ratings.data[element.userId] = {};
-                ratings.data[element.userId][element.movieId] = {
-                    rating: Number.parseFloat(element.rating)
+                ratingsByUser.data[element.userId] = {};
+                ratingsByUser.data[element.userId][element.movieId] = {
+                    rating: Number.parseFloat(element.rating),
+                    timestamp: element.timestamp
+                };
+            }
+            if (element.movieId in ratingsByMovie.data) {
+                ratingsByMovie.data[element.movieId][element.userId] = {
+                    rating: Number.parseFloat(element.rating),
+                    timestamp: element.timestamp
+                };
+            } else {
+                ratingsByMovie.data[element.movieId] = {};
+                ratingsByMovie.data[element.movieId][element.userId] = {
+                    rating: Number.parseFloat(element.rating),
+                    timestamp: element.timestamp
                 };
             }
         });
     })
     .then(() => {
-        console.log('Writing to file');
-        fs.writeFile('src/data/ratings_data.json', JSON.stringify(ratings), (err) => {
+        console.log('Writing to files');
+        fs.writeFile('src/data/ratings_data_by_user.json', JSON.stringify(ratingsByUser), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        fs.writeFile('src/data/ratings_data_by_movie.json', JSON.stringify(ratingsByMovie), (err) => {
             if (err) {
                 console.log(err);
             }
